c24b07444db2ce057c3f9483de135ef6934478c5
TheOneChiefRaka <rjmoore9361@gmail.com>
2025-10-27 21:46:12 -0500

    Created Util
    Created Utils file

---
+++ code/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello, World!");
+        float number = 3.0f;
+        float result = Utils.pow2(number);
+        System.out.println("The square of " + number + " is " + result);
+    }
+}
---
+++ code/Utils.java
@@ -0,0 +1,7 @@
+public class Utils {
+    private Utils() {} // Private constructor to prevent instantiation
+    
+    public static float pow2(float x) {
+        return x * x;
+    }
+}
